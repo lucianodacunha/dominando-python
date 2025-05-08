592523a790930b76db365f2c45b0eeaa7b152f2d
Luciano <lucianodacunha.net@gmail.com>
2025-05-07 22:03:33 -0300

    Menu e cadastro de autores. Exercicios.

--- modulo-01/aulas/aula_02.py
+++ modulo-01/aulas/aula_02.py
@@ -4,8 +4,27 @@ A partir dos valores da base e altura de um triângulo, calcular e exibir sua
 Fórmula: (base * altura) / 2.
 """
 
-base = float(input("Informe o valor da base do triângulo: "))
-altura = float(input("Informe o valor da altura do triângulo: "))
-area = (base * altura) / 2
+# base = float(input("Informe o valor da base do triângulo: "))
+# altura = float(input("Informe o valor da altura do triângulo: "))
+# area = (base * altura) / 2
 
-print(f"Área do tringulo: {area:.2f}.")
+# print(f"Área do tringulo: {area:.2f}.")
+
+"""
+Armazenar dez números em uma lista. Exibir os valores na ordem inversa à da
+digitação.
+"""
+
+# Método 1
+lista = list(range(1, 11))
+for i in lista:
+    print(lista[len(lista) - i], end=" ")
+
+print()
+
+# Método 2
+lista.reverse()
+for i in lista:
+    print(i, end=" ")
+
+print()
--- modulo-01/projeto/livraria/main.py
+++ modulo-01/projeto/livraria/main.py
@@ -5,13 +5,39 @@ from rich.prompt import Prompt
 console = Console()
 
 
-def main() -> None:
+class Autor:
+    serial: int = 0
 
-    categorias: list[str] = list()
-    editoras: list[str] = list()
-    autores: list[str] = list()
-    livros: list[str] = list()
+    def __init__(self, nome: str, biografia: str) -> None:
+        self._id = Autor.get_id()
+        self._nome = nome
+        self._biografia = biografia
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def nome(self) -> str:
+        return self._nome
     
+    @property
+    def biografia(self) -> str:
+        return self._biografia
+
+    @classmethod
+    def get_id(cls):
+        Autor.serial += 1
+        return Autor.serial
+    
+    def __str__(self) -> str:
+        return f"{str(id).zfill(2)} - {self.nome}"
+
+
+autores: list[Autor] = list()
+
+def main() -> None:
+  
     menu_principal()
     console.input("\n[red]Pressione enter para finalizar...")
     console.clear()
@@ -112,9 +138,9 @@ def menu_autores() -> None:
         opcao = console.input("\nInforme a opção desejada: ")
         match opcao:
             case "1":
-                pass
+                listar_autores()
             case "2":
-                pass
+                cadastrar_autores()
             case "3":
                 pass
             case "4":
@@ -125,6 +151,28 @@ def menu_autores() -> None:
                 console.input("\n[red]Opção inválida. Pressione enter para " + 
                                 "continuar... ")
 
+
+def listar_autores() -> None:
+        console.clear()
+        console.rule(title="Listagem de Autores", align="center")
+        console.print(f"{'Id'.rjust(2)} | {'Nome do Autor'.ljust(30)}")
+        for autor in autores:
+            console.print(f"{str(autor.id).rjust(2)}" + 
+                          f" | {autor.nome.ljust(30)}")
+        console.rule(align="center")    
+        console.input("\nPressione enter para continuar...")
+
+
+def cadastrar_autores() -> None:
+        console.clear()
+        console.rule(title="Cadastro de Autores", align="center")
+        nome = console.input("\nInforme o nome do autor: ")
+        bio = console.input("Informe a biografia do autor: ")
+        autor = Autor(nome=nome, biografia=bio)
+        autores.append(autor)
+        console.rule(align="center")    
+        console.input("\nAutor cadastrado com sucesso. "
+                      "Pressione enter para continuar...")
 
 def menu_livros() -> None:
     while True:
